669df6694f2973d8a3da2f8781a0f4a65d4a2378
Jerry Wan <craftersol728@gmail.com>
2023-02-05 15:04:33 -0800

    added access and updated connection file with .env login info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
-const Sequelize = require('sequelize');
-//This is a JavaScript code using the Sequelize library to connect to a MySQL database. The code starts by requiring the Sequelize library and the dotenv library, which loads environment variables from a .env file.
 require('dotenv').config();
-//Then, the code creates a new Sequelize instance, and connects to either a JAWSDB database (if the JAWSDB_URL environment variable is set) or a local MySQL database. The connection details such as database name, username, password, host, and dialect are stored in environment variables defined in a .env file.
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    });
-//Finally, the sequelize instance is exported so that it can be used in other parts of the application.
+
+const Sequelize = require('sequelize');
+
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+
+   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  });
+
+}
+
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helper = require('./utils/helper');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -6,18 +12,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helper });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
 const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'omegalol',
+  cookie: {
+    maxAge: 150
+  },
+  rolling: true,
+  resave: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
 
 app.use(session(sess));
 
-app.use(routes);
-
+app.use(function(req, res, next) {
+    // Perform some operation on the request and response objects
+    next();
+  });
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
